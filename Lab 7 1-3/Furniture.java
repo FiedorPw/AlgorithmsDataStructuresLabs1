764e3b720dbefa712f7b2b8308abff1d67312c46
mfiedorc <01168927@pw.edu.pl>
2021-11-30 13:58:58 +0100

    Upload New File

---
+++ Lab 7 1-3/Furniture.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Furniture {
+        private String description;
+
+    public Furniture(String description) {
+        this.description = description;
+    }
+
+    public String getdescription(){
+        return description;
+    }
+}
