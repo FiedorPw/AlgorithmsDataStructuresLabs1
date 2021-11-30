7bfcd5516ad9883422c2e08b0d81530b098d785d
mfiedorc <01168927@pw.edu.pl>
2021-11-30 13:59:24 +0100

    Upload New File

---
+++ Lab 7 1-3/MicroUSOSSystem.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class MicroUSOSSystem {
+    public static void main(String... args) {
+        Person os = new Teacher("Krystian", "Ignasiak", "WEiTI", "IRiTM", 450);
+        os = new Student("Aneta", "Kowalska", "WEiTI", "Cyberbezpieczeństwo");
+    }
+
+}
