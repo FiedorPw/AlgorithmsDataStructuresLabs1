e63d95aa355db2638c4d0e5ff71c928f2bae2535
mfiedorc <01168927@pw.edu.pl>
2021-11-30 13:59:39 +0100

    Upload New File

---
+++ Lab 7 1-3/Student.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Student extends Person{
+    private static String imie;
+    private static String Nazwisko;
+    private static String Wydział;
+    private static String Instytut;
+    private static String Kierunek;
+
+    public Student(String name,String surname,String dział,String koju){
+
+        imie = name;
+        Nazwisko = surname;
+        Wydział = dział;
+
+        Kierunek = koju;
+    }
+
+
+
+
+
+}
