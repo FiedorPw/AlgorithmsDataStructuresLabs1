9600312096bace5c56f45cb25f04c0713297bcc3
mfiedorc <01168927@pw.edu.pl>
2021-12-15 00:15:57 +0100

    Add new file

---
+++ Machiny/Main.java
@@ -0,0 +1,16 @@
+import static java.lang.Math.floor;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        Machine piec = new Machine(new int[]{2, 3, 2});
+        Factory.calculatingProductionSpeed();
+        Factory.produce();
+
+        }
+
+
+}
