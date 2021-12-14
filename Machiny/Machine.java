0c111208f10a5c7482b55f225d87d105d83cc579
mfiedorc <01168927@pw.edu.pl>
2021-12-15 00:15:05 +0100

    Add new file

---
+++ Machiny/Machine.java
@@ -0,0 +1,11 @@
+public class Machine {
+    static private int[] Speed;
+    public Machine(int[] Speed ) {
+        this.Speed = Speed;
+    }
+
+    static public int[] getSpeed() {
+        return Speed;
+    }
+}
+
